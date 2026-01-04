6dcf25175f2f1f8cea965c53cc0123028123a717
Vincent Battez <vincent.battez.pro@gmail.com>
2026-01-04 17:36:03 +0100

    refactor: lint

--- src/utils/jsType.d.ts
+++ src/utils/jsType.d.ts
@@ -1,12 +1,12 @@
+type Entries<T> = Array<
+  {
+    [K in keyof T]: [K, T[K]];
+  }[keyof T]
+>;
+
 declare global {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface ObjectConstructor {
     entries<T>(o: T): Entries<T>;
   }
 }
-
-type Entries<T> = Array<
-  {
-    [K in keyof T]: [K, T[K]];
-  }[keyof T]
->;
